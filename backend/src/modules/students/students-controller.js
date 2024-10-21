5fb7209a2c343c727368eb4b8774b68fd201dccc
lucaspdude <lucaspdude@gmail.com>
2024-10-21 11:41:50 -0300

    feat(student-controller): implements student controller

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -1,35 +1,67 @@
 const asyncHandler = require("express-async-handler");
-const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
+const {
+  getAllStudents,
+  addNewStudent,
+  getStudentDetail,
+  setStudentStatus,
+  updateStudent,
+} = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+  const students = await getAllStudents();
+  res.json({
+    students,
+  });
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+  const { id: authorId } = req.user;
+  const payload = req.body;
+  const student = await addNewStudent({
+    ...payload,
+    authorId,
+  });
+  res.json({
+    student,
+  });
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+  const { id: authorId } = req.user;
+  const payload = req.body;
+  const student = await updateStudent(payload);
+  res.json({
+    student,
+  });
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
-
+  const { id } = req.params;
+  const student = await getStudentDetail(id);
+  res.json({
+    student,
+  });
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
-
+  const { id: currentUserId, role: currentUserRole } = req.user;
+  const { id: studentId } = req.params;
+  const { status } = req.body;
+  const payload = {
+    userId: studentId,
+    reviewerId: currentUserId,
+    status,
+  };
+  const message = await setStudentStatus(payload);
+  res.json({
+    message,
+  });
 });
 
 module.exports = {
-    handleGetAllStudents,
-    handleGetStudentDetail,
-    handleAddStudent,
-    handleStudentStatus,
-    handleUpdateStudent,
+  handleGetAllStudents,
+  handleGetStudentDetail,
+  handleAddStudent,
+  handleStudentStatus,
+  handleUpdateStudent,
 };
